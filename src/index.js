42ead2d1aaf9a5b4e5182f1531503eaf4c1d5a58
Sarthak Saklecha <sarthaksaklecha1@gmail.com>
2021-07-15 18:30:43 +0530

    fixed indentation

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 
